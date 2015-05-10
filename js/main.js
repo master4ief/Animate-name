358268024cac35caeeec1f4a3d8de6c08122d570
master4ief <lpi.soft.comalex@gmail.com>
2015-05-10 13:57:40 +0300

    second commit

--- js/main.js
+++ js/main.js
@@ -7,3 +7,12 @@ var purple = [280, 50, 60];
 var myName = "master4ief";
 var letterColors = [red, orange, green, blue, purple];
 
+if(10 < 3) {
+    bubbleShape = 'square';
+}
+else {
+    bubbleShape = 'circle';
+}
+
+drawName(myName, letterColors);
+bounceBubbles();
